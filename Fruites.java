fa85369cc6954365a64955cf015e228f4e9428bc
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-08-08 16:22:00 +0530

    java program

---
+++ Fruites.java
@@ -0,0 +1,22 @@
+class Fruites{
+public static int numberOfColors(){
+int Colors = 60;
+return Colors;
+}
+public static int numberOfTypes(){
+int Types = 300;
+return Types;
+}
+public static int numberOfSizes(){
+int Sizes = 50;
+return Sizes;
+}
+public static int numberOfTaste(){
+int Teste = 2;
+return Teste;
+}
+public static int numberOfSeeds(){
+int Seeds = 4;
+return Seeds;
+}
+}
